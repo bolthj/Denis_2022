d11a466a0d6217a2ec6f2803a87f17aa120794cd
Denis <bolthj@mail.ru>
2022-08-18 15:10:22 +0300

    start_files

--- Denis_2022/blog/forms.py
+++ Denis_2022/blog/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class CommentForm(forms.Form):
     author = forms.CharField(
-        max_lenght=60,
+        max_length=60,
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': 'Ваше имя'
